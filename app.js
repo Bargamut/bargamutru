fe402258c7e5fa21717ac585d75480ac74588201
Paul Petrov <paul@bargamut.ru>
2020-02-28 21:57:46 +0300

    Add app.js

---
+++ app.js
@@ -0,0 +1,67 @@
+const $appContainer = document.querySelector(`.app`);
+
+$appContainer.innerHTML = `
+	<main class="container bg-dark">
+		<header>
+			<nav class="navbar navbar-dark bg-dark">
+				<a class="navbar-brand" href="/">Bargamut</a>
+
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav mr-auto">
+						<li class="nav-item active">
+							<a class="nav-link" href="/">Home <span class="sr-only">current</span></a>
+						</li>
+						<li class="nav-item active">
+							<a class="nav-link disabled" href="https://ancientlores.ru/" tabindex="-1" aria-disabled="true">Ancient Lores</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</header>
+
+		<section class="card-deck">
+			<section class="card vk">
+				<img src="..." alt="..." class="card-img">
+
+				<div class="card-body">
+					<h5 class="card-title">ВКонтакте</h5>
+					<p class="card-text">Здесь меня можно найти во ВКонтакте</p>
+				</div>
+			</section>
+
+			<section class="card fb">
+				<img src="..." alt="..." class="card-img">
+
+				<div class="card-body">
+					<h5 class="card-title">Facebook</h5>
+					<p class="card-text">Здесь меня можно найти в Facebook</p>
+				</div>
+			</section>
+
+			<section class="card instagram">
+				<img src="..." alt="..." class="card-img">
+
+				<div class="card-body">
+					<h5 class="card-title">Instagram</h5>
+					<p class="card-text">Здесь меня можно найти в Instagram</p>
+				</div>
+			</section>
+			
+			<section class="card tw">
+				<img src="..." alt="..." class="card-img">
+
+				<div class="card-body">
+					<h5 class="card-title">Twitter</h5>
+					<p class="card-text">Здесь меня можно найти в Twitter</p>
+				</div>
+			</section>
+		</section>
+
+		<footer>
+			2020 &copy; Paul "Bargamut" Petrov. All rights reserved.
+		</footer>
+	</main>`;
