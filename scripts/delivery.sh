ecd5381e6de3ef8436163423721177688167c09e
Paul Petrov <paul@bargamut.ru>
2020-03-24 19:35:31 +0300

    Fix 14

--- scripts/delivery.sh
+++ scripts/delivery.sh
@@ -4,14 +4,14 @@ CONTAINER_OPTIONS='-d -p 3000:80' # $1
 CONTAINER_NAME='bargamut-site' # $2
 IMAGE_NAME='bargamut/bargamut-site:latest' # $3
 
-if [ ! "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
+if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
 	docker stop $CONTAINER_NAME
 
 	if [ "$(docker ps -aq -f status=exited -f name=$CONTAINER_NAME)" ]; then
 			# cleanup
 			docker rm $CONTAINER_NAME
 	fi
-
-	# run your container
-	docker run $CONTAINER_OPTIONS --name $CONTAINER_NAME $IMAGE_NAME
 fi
+
+# run your container
+docker run $CONTAINER_OPTIONS --name $CONTAINER_NAME $IMAGE_NAME
