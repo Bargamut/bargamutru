24cefe76ac49b94919efc523bdb20aefd4d3eac8
Paul Petrov <paul@bargamut.ru>
2020-03-24 19:25:47 +0300

    Fix 11

--- scripts/delivery.sh
+++ scripts/delivery.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-CONTAINER_OPTIONS = '-d -p 3000:80' # $1
-CONTAINER_NAME = 'bargamut-site' # $2
-IMAGE_NAME = 'bargamut/bargamut-site:latest' # $3
+CONTAINER_OPTIONS='-d -p 3000:80' # $1
+CONTAINER_NAME='bargamut-site' # $2
+IMAGE_NAME='bargamut/bargamut-site:latest' # $3
 
 if [ ! "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
 	docker stop $CONTAINER_NAME
