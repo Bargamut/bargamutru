fe27b8525c880d0108e03946c74d4dc385d9ff00
Paul Petrov <paul@bargamut.ru>
2020-03-24 19:06:44 +0300

    Fix 8

--- scripts/delivery.sh
+++ scripts/delivery.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker stop bargamut-site-contaner
-docker rm bargamut-site-contaner
+docker stop bargamut-site-contaner || \
+docker rm bargamut-site-contaner || \
 docker run --name bargamut-site -p 3000:80 -d bargamut-site-contaner:latest
