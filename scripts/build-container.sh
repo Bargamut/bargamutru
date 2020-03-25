58390737c5e8c46c12bbead2e54d76f78117424a
Paul Petrov <paul@bargamut.ru>
2020-03-25 17:52:49 +0300

    Add clean dangling images script

--- scripts/build-container.sh
+++ scripts/build-container.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 docker build -t bargamut/bargamut-site:latest .
-# Garbage collect for images after multi-stage builds. See https://habr.com/ru/post/304134/ (Eng: https://www.projectatomic.io/blog/2015/07/what-are-docker-none-none-images/)
-docker rmi $(docker images -q -f dangling=true)
