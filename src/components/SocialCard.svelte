8d584d6fc7cbb4708696461e0fe8a80ca6c91e9d
Paul Petrov <paul@bargamut.ru>
2020-03-26 18:40:05 +0300

    Add SocialCard component

---
+++ src/components/SocialCard.svelte
@@ -0,0 +1,16 @@
+<script>
+	export let id;
+	export let img;
+	export let body;
+</script>
+
+<section class="card {id}">
+	<img src="{img.src}" alt="{img.alt}" class="card-img">
+
+	<div class="card-body">
+		<h5 class="card-title">{body.title}</h5>
+		<p class="card-text">{body.text}</p>
+		
+		<a href="{body.link.href}" class="btn btn-block btn-primary">{body.link.caption}</a>
+	</div>
+</section>
