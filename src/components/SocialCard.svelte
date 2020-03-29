5a03f8c6f4baedafb17024882ab4d36f6e5c8dd8
Paul Petrov <paul@bargamut.ru>
2020-03-30 01:57:16 +0300

    Switch to dark theme

--- src/components/SocialCard.svelte
+++ src/components/SocialCard.svelte
@@ -4,13 +4,13 @@
 	export let body;
 </script>
 
-<section class="card {id}">
+<section class="card {id} bg-transparent text-light border-0">
 	<img src="{img.src}" alt="{img.alt}" class="card-img">
 
 	<div class="card-body">
 		<h5 class="card-title">{body.title}</h5>
 		<p class="card-text">{body.text}</p>
 		
-		<a href="{body.link.href}" class="btn btn-block btn-primary">{body.link.caption}</a>
+		<a href="{body.link.href}" class="btn btn-block btn-light">{body.link.caption}</a>
 	</div>
 </section>
